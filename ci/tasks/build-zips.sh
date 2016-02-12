52e81b67411127fb112ad4c2fcc9d9a891bf3fd4
Dr Nic Williams <drnicwilliams@gmail.com>
2016-02-12 14:42:26 +1000

    build both buildpack zips

--- ci/tasks/build-zips.sh
+++ ci/tasks/build-zips.sh
@@ -3,7 +3,8 @@
 cd repo
 
 BUNDLE_GEMFILE=cf.Gemfile bundle install
-BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --uncached
+BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --uncached --use-custom-manifest manifest.yml
+BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --cached --use-custom-manifest manifest-cached.yml
 
 mkdir -p ../zips
 mv *.zip ../zips
