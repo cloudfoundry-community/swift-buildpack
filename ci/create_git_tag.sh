0acb52e62c239cbd5fbe550692c4a4edc148a7f7
Brian Hernandez <bhernand@us.ibm.com>
2016-10-25 12:40:07 -0500

    Test build

--- ci/create_git_tag.sh
+++ ci/create_git_tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then    
+#if [ "$TRAVIS_BRANCH" == "master" ]; then    
   cd $TRAVIS_BUILD_DIR
   gem install bundler
   bundle install --gemfile=cf.Gemfile
@@ -13,4 +13,4 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   mv swift_buildpack-cached-v$VERSION.zip buildpack_swift_v$VERSION-$DATE.zip
   git tag $VERSION -f
   git push https://$GITHUB_USER:$GITHUB_PASS@github.com/IBM-Swift/swift-buildpack.git --tags --force --quiet
-fi
+#fi
