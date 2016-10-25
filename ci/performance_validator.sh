854103f2ff227487f14d3e4299706946599ad506
Brian Hernandez <bhernand@us.ibm.com>
2016-10-25 12:18:10 -0500

    review comments

--- ci/performance_validator.sh
+++ ci/performance_validator.sh
@@ -6,31 +6,48 @@ APPLICATION_DIR=$1
 APPLICATION_TIMEOUT=$2
 TRAVIS_BRANCH=$3
 TIMES_TO_REPEAT=$4
+APPLICATION_REPUSH_TIMEOUT=$5
+
+push_and_optionally_delete () {
+	DELETE=$1
+	TIMEOUT=$2
+
+	local RETVAL=1
+
+	if [ "$DELETE" = true ]; then
+		echo "Clearing out any previous instances of $APPLICATION_DIR"
+		cf delete $APPLICATION_DIR -f
+	fi
+
+	echo "$APPLICATION_DIR threshold value is $TIMEOUT"
+	echo
+	for num in `seq 1 $TIMES_TO_REPEAT`; do
+		START_TIME=$SECONDS
+		cf push -b https://github.com/IBM-Swift/swift-buildpack.git#$TRAVIS_BRANCH
+		ELAPSED_TIME=$(($SECONDS - $START_TIME))
+		if [ "$DELETE" = true ]; then
+			cf delete $APPLICATION_DIR -f
+		fi
+		echo "$APPLICATION_DIR took $ELAPSED_TIME seconds."
+		if [ "$ELAPSED_TIME" -lt "$TIMEOUT" ]; then
+			echo "Application was under threshold value."
+			RETVAL=0
+			break
+		fi
+		echo "$APPLICATION_DIR took longer than the threshold value."
+	done
+
+	echo "$RETVAL"	
+}
 
 cd $APPLICATION_DIR
 
-passed=1
+passed=$(push_and_optionally_delete true $APPLICATION_TIMEOUT)
 
-echo "Clearing out any previous instances of $APPLICATION_DIR"
-cf delete $APPLICATION_DIR -f
-
-echo "$APPLICATION_DIR threshold value is $APPLICATION_TIMEOUT"
-echo
-
-for num in `seq 1 $TIMES_TO_REPEAT`; do
-	START_TIME=$SECONDS
-	cf push -b https://github.com/IBM-Swift/swift-buildpack.git#$TRAVIS_BRANCH
-	ELAPSED_TIME=$(($SECONDS - $START_TIME))
-	cf delete $APPLICATION_DIR -f
-	echo "$APPLICATION_DIR took $ELAPSED_TIME seconds."
-	if [ "$ELAPSED_TIME" -lt "$APPLICATION_TIMEOUT" ]; then
-		echo "Application was under threshold value."
-		passed=0
-		break
-	fi
-	echo "$APPLICATION_DIR took longer than the threshold value."
-done
+passed_repush=$(push_and_optionally_delete false $APPLICATION_REPUSH_TIMEOUT)
 
 cd ..
 
-exit $passed
+! (( $passed | $passed_repush ));
+
+exit $?
