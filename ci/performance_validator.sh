0c81ee23199046ac55c07f9a88434fdccf9bd070
rolivieri <roliv@us.ibm.com>
2016-10-31 17:00:49 -0500

    Minor refactoring to travis ci files.

--- ci/performance_validator.sh
+++ ci/performance_validator.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
 
 set -ev
 
@@ -13,23 +28,27 @@ push_application () {
 	local RETVAL=1
 
 	if [ "$DELETE_FLAG" = true ]; then
-		echo "Clearing out any previous instances of $APPLICATION_DIR"
+		echo "Executing cf push tests..."
+		echo "Clearing out any previous instances of: $APPLICATION_DIR"
 		cf delete $APPLICATION_DIR -f
+	else
+		echo "Executing cf re-push tests..."
 	fi
 
 	echo "$APPLICATION_DIR threshold value is: $TIMEOUT"
-	echo	
+	echo
+
 	for num in `seq 1 $TIMES_TO_REPEAT`; do
 		START_TIME=$SECONDS
 		cf push -b https://github.com/IBM-Swift/swift-buildpack.git#$TRAVIS_BRANCH
 		ELAPSED_TIME=$(($SECONDS - $START_TIME))
-		
+
 		if [ "$DELETE_FLAG" = true ]; then
 			cf delete $APPLICATION_DIR -f
 		fi
-		
+
 		echo "$APPLICATION_DIR took $ELAPSED_TIME seconds."
-		
+
 		if [ "$ELAPSED_TIME" -lt "$TIMEOUT" ]; then
 			echo "Application was under threshold value."
 			RETVAL=0
@@ -38,7 +57,7 @@ push_application () {
 
 		echo "$APPLICATION_DIR took longer than the threshold value."
 	done
-	echo "$RETVAL"	
+	echo "$RETVAL"
 }
 
 cd $APPLICATION_DIR
