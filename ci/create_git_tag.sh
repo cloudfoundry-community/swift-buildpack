25357233dc6ce791e7fbd525cf986a436ece5d3d
Brian Hernandez <bhernand@us.ibm.com>
2016-11-03 08:58:48 -0500

    Changed the date format to hour/minute instead of miunute/second to better aid in unique names

--- ci/create_git_tag.sh
+++ ci/create_git_tag.sh
@@ -25,7 +25,7 @@ if [ "$DEPLOY_FLAG" == "true" ]; then
   BUNDLE_GEMFILE=cf.Gemfile bundle update
   BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --cached --use-custom-manifest manifest.yml
   export VERSION=`cat VERSION`
-  export DATE=`date +"%Y%m%d-%M%S"`
+  export DATE=`date +"%Y%m%d-%H%M"`
   mv swift_buildpack-cached-v$VERSION.zip buildpack_swift_v$VERSION-$DATE.zip
   git tag $VERSION -f
   git push https://$GITHUB_USER:$GITHUB_PASS@github.com/IBM-Swift/swift-buildpack.git --tags --force --quiet
